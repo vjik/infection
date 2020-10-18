0f52dd6f27d6c84bec65a0256497cbdc82fcbc05
Oleg Zhulnev <plbsid@gmail.com>
2020-10-18 22:36:35 +0300

    Restore DecrementInteger test cases which were dropped by mistake

--- tests/phpunit/Mutator/Number/DecrementIntegerTest.php
+++ tests/phpunit/Mutator/Number/DecrementIntegerTest.php
@@ -192,6 +192,214 @@ PHP
             ,
         ];
 
+        yield 'It does not decrement zero when it is compared as greater than count() on the right side' => [
+            <<<'PHP'
+<?php
+
+if (0 > count($a)) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is compared as more or equal than count()' => [
+            <<<'PHP'
+<?php
+
+if (count($a) >= 0) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It doest not decrement zero when it is compared as less than count()' => [
+            <<<'PHP'
+<?php
+
+if (count($a) < 0) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It does decrement when compared against a variable function' => [
+            <<<'PHP'
+<?php
+
+if ($foo === 0) {
+    echo 'bar';
+}
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+if ($foo === -1) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It decrements zero when it is compared any other, not count() function' => [
+            <<<'PHP'
+<?php
+
+if (abs($a) === 0) {
+    echo 'bar';
+}
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+if (abs($a) === -1) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It doest not decrements zero when it is compared as less or equal than count()' => [
+            <<<'PHP'
+<?php
+
+if (count($a) <= 0) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It decrements zero' => [
+            <<<'PHP'
+<?php
+
+$a = 0;
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+$a = -1;
+PHP
+            ,
+        ];
+
+        yield 'It increments a negative integer' => [
+            <<<'PHP'
+<?php
+
+if ($foo === -10) {
+    echo 'bar';
+}
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+if ($foo === -9) {
+    echo 'bar';
+}
+PHP
+            ,
+        ];
+
+        yield 'It decrements an assignment' => [
+            <<<'PHP'
+<?php
+
+$foo = 10;
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+$foo = 9;
+PHP
+        ];
+
+        yield 'It decrements an assignment of 0' => [
+            <<<'PHP'
+<?php
+
+$foo = 0;
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+$foo = -1;
+PHP
+        ];
+
+        yield 'It does not decrement an assignment of 1' => [
+            <<<'PHP'
+<?php
+
+$foo = 1;
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is being compared as identical with result of grapheme_strlen()' => [
+            <<<'PHP'
+<?php
+
+if (grapheme_strlen($a) === 0) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is being compared as identical with result of iconv_strlen()' => [
+            <<<'PHP'
+<?php
+
+if (iconv_strlen($a) === 0) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is being compared as identical with result of mb_strlen()' => [
+            <<<'PHP'
+<?php
+
+if (mb_strlen($a) === 0) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is being compared as identical with result of sizeof()' => [
+            <<<'PHP'
+<?php
+
+if (sizeof($a) === 0) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement zero when it is being compared as identical with result of strlen()' => [
+            <<<'PHP'
+<?php
+
+if (strlen($a) === 0) {
+    echo 'bar';
+}
+PHP
+        ];
+
+        yield 'It does not decrement when it is accessed zero index of an array' => [
+            <<<'PHP'
+<?php
+$b = $a[0];
+PHP
+        ];
+
         yield 'It does not decrement limit argument of preg_split function when it equals to 0' => [
             <<<'PHP'
 <?php
